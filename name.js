78cd04a5097e839995fba0ba29f66c7a492f9bd8
mide-dev <ayo4real6@gmail.com>
2022-06-28 17:20:30 +0100

    first commit -1

---
+++ name.js
@@ -0,0 +1,12 @@
+var firstName = 'Ayomide';
+var middleName = 'Williams';
+var lastName = 'Oke';
+var height = '1.86m'
+var country = 'Nigeria'
+
+name = firstName + ' ' + middleName + ' ' + lastName;
+
+document.write('Name:' + ' ' + name  + '<br/>');
+document.write('Height:' + ' ' + height + '<br>'); 
+document.write('Country:' + 
+' ' + country);
